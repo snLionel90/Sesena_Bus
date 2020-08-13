f4d73475c7db38c28072a9a93f25ae324b2b7c6d
Lionel Sanchez <58988565+snLionel90@users.noreply.github.com>
2020-08-13 18:18:21 +0200

    bacground,visit counter, new lines and icons added

---
+++ js/visitor_counter.js
@@ -0,0 +1,39 @@
+function getexpiredate(nodays){
+    var UTCstring;
+    Today = new Date();
+    nomilli = Date.parse(Today);
+    Today.setTime(nomilli+nodays*24*60*60*1000);
+
+    UTCstring = Today.toUTCstring();
+    return UTCstring;
+}
+
+function getcookie(cookiename){
+    var cookiestring = "" +document.cookie;
+    var index1= cookiestring.indexOf(cookiename);
+    if (index1==-1 || cookiename==""){
+        return "";
+    } 
+
+    var index2= cookiestring.indexOf(";", index1);
+    if (index2==-1){
+        index2=cookiestring.length;
+        return unescape(cookiestring.substring(index1+cookiename.length+arguments.index1));
+    }
+}
+
+function setcookie ( name, value,duration){
+    cookiestring= name+"="+escape(value)+ ";EXPIRES="+getexpiredate(duration);
+    document.cookie=cookiestring;
+    if (!getcookie(name)){
+        return false;
+    }
+    else{
+        return true;
+    }
+}
+count++;
+document.write("Gracias por haber visto/visitado esta pagina "+ count+" veces");
+y = setcookie("contador",count,1);
+//
+// -->
